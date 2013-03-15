3ae3ec3be57da8cfefdd620ea1a4ebfe2f4fb2af
Xinchao Liu <leo_liu@csu.fullerton.edu>
2013-03-15 12:58:16 -0700

    Initial Commit

---
+++ Discount Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Discount Calculator
+//
+//  Created by Xinchao Liu on 3/15/13.
+//  Copyright (c) 2013 Xinchao Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Discount Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Discount Calculator
+//
+//  Created by Xinchao Liu on 3/15/13.
+//  Copyright (c) 2013 Xinchao Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
